0b67d33f5a9cfa0b4d491a2b207f8de34f0149e1
MaxGoxt <m.gomezcoitino@gmail.com>
2024-09-10 06:10:34 -0300

    Refactorizo código: se substituye el uso de _process por _input descarto el use de Documents.txt para ahorrar código y procesamiento modularizo codigo de Draggable.gd en funciones se eliminan algunos warnings

---
+++ Prompts/Documents.gd
@@ -0,0 +1,14 @@
+@export var docs = [
+	"(all)The creator of this work has requested [] compliance, which requires proper attribution for any usage.",
+	"(SA)This work is protected under a license that mandates all derivative works to be shared alike. Any adaptation must adhere to this condition.",
+	"(ND)You are permitted to use this work as long as it remains unaltered and attributed correctly. Any modifications are not allowed.",
+	"(NC)Usage of this content is limited to non-commercial purposes only. Ensure that any adaptations are not used for commercial gain.",
+	"(BYNC)To comply with this license, you must provide attribution and ensure that the work is not used for commercial purposes or modified.",
+	"(BYND)This work may be used and shared as long as it's not altered and proper credit is given to the creator. Commercial use is prohibited.",
+	"(BY)An individual used the content in a promotional video without proper attribution. The original creator has filed a lawsuit for failing to credit them as required by the license.",
+	"(SANC)A company adapted the content and sold it as part of a new product, ignoring the requirement to share derivative works under the same conditions. Legal action is being taken for non-compliance.",
+	"(NDNC)The work was published on a commercial website after being altered. The creator is pursuing legal action for breaching the no-derivative and non-commercial terms of the license.",
+	"(SAND)A non-profit organization used the content in a fundraising campaign, modifying it and failing to share the new version under the same license. The creator is suing for the misuse of the license terms.",
+	"(NDNC)A blog post included an edited version of the licensed content without permission for commercial purposes. The original creator is seeking damages for unauthorized use and modifications.",
+	"(NC)The content was used in an advertisement, violating the license's non-commercial clause. The creator is filing a lawsuit for unauthorized commercial exploitation."
+]
--- Scenes/Cursor.gd
+++ Scenes/Cursor.gd
@@ -1,11 +1,11 @@
 extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN) # Se esconde el mouse al iniciar el juego
 
 
-func _input(evt):
+func _input(evt) -> void:
 	# Setea la posición del objeto Cursor a la posición del Mouse
 	global_position = get_global_mouse_position()
 	self.move_to_front()
