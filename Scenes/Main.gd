9c54ed14cce50c2d0ffdcaab26177a01746c4aa1
MaxGoxt <m.gomezcoitino@gmail.com>
2024-09-24 11:57:57 -0300

    Refactorizo código

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -33,67 +33,36 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 
 
 func validate_player_responses():
-	print(validate_checks())
+	print(generate_results())
 
 
-func validate_checks() -> Dictionary:
+func generate_results() -> Dictionary:
 		var results : Dictionary = {"rounds" : [], "total_errors" : 0, "total_oks" : 0}
-		
+
 		var user_res : Array = global.user_responses
 		var ok_res : Array = global.correct_responses
-		
+
 		for i in range(0, user_res.size()-1):
-			var err_count : int = 0
-			var ok_count : int = 0
-			
-			results["rounds"].append({
-				"license" : null,
-				"att" : null,
-				"sa" : null,
-				"nc" : null,
-				"nd" : null,
+			var result = {
+				"license" : user_res[i]["license"] == ok_res[i]["license"],
+				"att" : user_res[i]["att"] == ok_res[i]["att"],
+				"sa" : user_res[i]["sa"] == ok_res[i]["sa"],
+				"nc" : user_res[i]["nc"] == ok_res[i]["nc"],
+				"nd" : user_res[i]["nd"] == ok_res[i]["nd"],
+				"app" : user_res[i]["app"] == ok_res[i]["app"],
 				"errors" : 0,
 				"oks" : 0,
-			})
-			
-			if user_res[i]["license"] != ok_res[i]["license"]: 
-				err_count += 1 
-				results["rounds"][i]["license"] = true
-			else : 
-				results["rounds"][i]["license"] = false
-				ok_count += 1
-			
-			if user_res[i]["att"] != ok_res[i]["att"]:
-				err_count += 1 
-				results["rounds"][i]["att"] = true
-			else : 
-				results["rounds"][i]["att"] = false
-				ok_count += 1
-			
-			if user_res[i]["sa"] != ok_res[i]["sa"]:
-				err_count += 1 
-				results["rounds"][i]["sa"] = true
-			else : 
-				results["rounds"][i]["sa"] = false
-				ok_count += 1
-			
-			if user_res[i]["nc"] != ok_res[i]["nc"]:
-				err_count += 1 
-				results["rounds"][i]["nc"] = true
-			else : 
-				results["rounds"][i]["nc"] = false
-				ok_count += 1
-			
-			if user_res[i]["nd"] != ok_res[i]["nd"]:
-				err_count += 1 
-				results["rounds"][i]["nd"] = true
-			else : 
-				results["rounds"][i]["nd"] = false
-				ok_count += 1
-			
-			results["rounds"][i]["errors"] = err_count
-			results["rounds"][i]["oks"] = ok_count
-			results["total_errors"] += err_count
-			results["total_oks"] += ok_count
-		
+			}
+
+			result["oks"] = (int(result["license"]) + int(result["att"]) + 
+						   int(result["sa"]) + int(result["nc"]) + 
+						   int(result["nd"]) + int(result["app"]))
+
+			result["errors"] = 6 - result["oks"]
+
+			results["rounds"].append(result)
+
+			results["total_errors"] += result["errors"]
+			results["total_oks"] += result["oks"]
+
 		return results
