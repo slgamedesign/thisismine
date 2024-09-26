ae9452cd6666448f94f9a3b5bbb4582fb88a91c3
LuisPereira05 <luispereira.sl014@academico.ifsul.edu.br>
2024-09-26 17:20:26 -0300

    Score scene created

--- Scenes/score.gd
+++ Scenes/score.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
-
+@export var background : AnimatedSprite2D
+@export var engine : AudioStreamPlayer2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	background.frame = 68
+	background.play_backwards("default")
 	str(global.score)
 	pass # Replace with function body.
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if background.frame == 60:
+		engine.play()
 	pass
